567b92409be8503f299437e5f4a5c13dc7331c86
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-13 22:23:04 +0900

    Spring Item Service Application

--- src/main/java/com/codemos/springitemservice/SpringItemServiceApplication.java
+++ src/main/java/com/codemos/springitemservice/SpringItemServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringItemServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringItemServiceApplication.class, args);
 	}
-
 }
